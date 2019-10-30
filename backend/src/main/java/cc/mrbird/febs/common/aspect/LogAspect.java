0f7df6f5d038f132c2d9baf48959c843d213d154
Leo Yang <silverleo@163.com>
2019-10-30 17:18:32 +0800

    多环境切换配置

--- backend/src/main/java/cc/mrbird/febs/common/aspect/LogAspect.java
+++ backend/src/main/java/cc/mrbird/febs/common/aspect/LogAspect.java
@@ -14,6 +14,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,6 +28,7 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 @Aspect
 @Component
+@Profile({"dev", "test"})
 public class LogAspect {
 
     @Autowired
