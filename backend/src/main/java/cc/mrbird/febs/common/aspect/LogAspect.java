8f08f52652ef14739f226a1b2fc7da695330093b
Leo Yang <silverleo@163.com>
2019-10-30 19:02:16 +0800

    改进日志输出功能

--- backend/src/main/java/cc/mrbird/febs/common/aspect/LogAspect.java
+++ backend/src/main/java/cc/mrbird/febs/common/aspect/LogAspect.java
@@ -1,5 +1,6 @@
 package cc.mrbird.febs.common.aspect;
 
+import cc.mrbird.febs.common.annotation.Log;
 import cc.mrbird.febs.common.authentication.JWTUtil;
 import cc.mrbird.febs.common.properties.FebsProperties;
 import cc.mrbird.febs.common.utils.HttpContextUtil;
@@ -9,15 +10,18 @@ import cc.mrbird.febs.system.service.LogService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import com.google.gson.Gson;
 
 /**
  * AOP 记录用户操作日志
@@ -31,6 +35,9 @@ import javax.servlet.http.HttpServletRequest;
 @Profile({"dev", "test"})
 public class LogAspect {
 
+    /** 换行符 */
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
     @Autowired
     private FebsProperties febsProperties;
 
@@ -40,6 +47,41 @@ public class LogAspect {
     @Pointcut("@annotation(cc.mrbird.febs.common.annotation.Log)")
     public void pointcut() {
         // do nothing
+    }
+
+    @Before("pointcut()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 获取 @Log 注解的描述信息
+        String methodDescription = getAspectLogDescription(joinPoint);
+
+        // 打印请求相关参数
+        log.info("========================================== Start ==========================================");
+        // 打印请求 url
+        log.info("URL            : {}", request.getRequestURL().toString());
+        // 打印描述信息
+        log.info("Description    : {}", methodDescription);
+        // 打印 Http method
+        log.info("HTTP Method    : {}", request.getMethod());
+        // 打印调用 controller 的全路径以及执行方法
+        log.info("Class Method   : {}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+        // 打印请求的 IP
+        log.info("IP             : {}", request.getRemoteAddr());
+        // 打印请求入参
+        log.info("Request Args   : {}", new Gson().toJson(joinPoint.getArgs()));
+
+    }
+
+    /**
+     * 在切点之后织入
+     * @throws Throwable
+     */
+    @After("pointcut()")
+    public void doAfter() throws Throwable {
+        // 接口结束后换行，方便分割查看
+        log.info("=========================================== End ===========================================" + LINE_SEPARATOR);
     }
 
     @Around("pointcut()")
@@ -69,5 +111,32 @@ public class LogAspect {
             logService.saveLog(point, log);
         }
         return result;
+    }
+
+    /**
+     * 获取切面注解的描述
+     *
+     * @param joinPoint 切点
+     * @return 描述信息
+     * @throws Exception
+     */
+    public String getAspectLogDescription(JoinPoint joinPoint)
+            throws Exception {
+        String targetName = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] arguments = joinPoint.getArgs();
+        Class targetClass = Class.forName(targetName);
+        Method[] methods = targetClass.getMethods();
+        StringBuilder description = new StringBuilder("");
+        for (Method method : methods) {
+            if (method.getName().equals(methodName)) {
+                Class[] clazzs = method.getParameterTypes();
+                if (clazzs.length == arguments.length) {
+                    description.append(method.getAnnotation(Log.class).value());
+                    break;
+                }
+            }
+        }
+        return description.toString();
     }
 }
